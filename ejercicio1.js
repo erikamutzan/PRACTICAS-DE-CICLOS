538ba16480f869b13ab6deb13eb14f5de029f8a0
Erika Mutzan <erikamtzmutzan@gmail.com>
2025-02-02 22:46:27 -0600

    Implementacion de ejercicio 1 tarea

---
+++ ejercicio1.js
@@ -0,0 +1,20 @@
+/* Exercise #1 ( sugerencia join() )
+Copy the code below into your workspace:
+let arr = ["This", "is", "a", "sentence."];
+function printOutString() {
+ // your code here
+}
+printOutString();
+Complete the function to print out the string: This is a sentence. */
+
+let arr = ["This", "is", "a", "sentence."];
+
+function printOutString() {
+    let stringFinal ="";
+    for(i = 0; i < arr.length; i++) {
+        let stringElementArray = arr[i];
+        stringFinal = stringFinal + " " + stringElementArray
+    }
+    return stringFinal;
+}
+console.log(printOutString());
